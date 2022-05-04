5daff555e6e5fc4ba6f17ce42c9bdbfee98e96b9
Amjad <amjadmaqsuma@gmail.com>
2022-05-04 20:06:16 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+const form = document.querySelector("#create-task-form")
+    const Task = document.querySelector("#new-task-description")
+     const addedTask = document.querySelector("#tasks")
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ 
+   
+      form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        addTask()
+        form.reset()
+       })  
+
+      });
+
+
+
+const addTask = () => {
+
+  let li = document.createElement('li') ;
+  let btn = document.createElement('button')
+  li.textContent = `${Task.value} `
+  addedTask.appendChild(li)
+  btn.textContent = `X`
+  addedTask.lastElementChild.appendChild(btn)
+  btn.addEventListener("click", deleteTask)
+  
+  }
+const deleteTask = (e) => {
+  e.target.parentNode.remove()}
+
